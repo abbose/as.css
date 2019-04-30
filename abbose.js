20acc02add86399ae2d7e2d7b4b3dc8553977e35
abbose <43781525+abbose@users.noreply.github.com>
2019-04-30 21:38:38 +0300

    Create abbose.js

---
+++ abbose.js
@@ -0,0 +1,30 @@
+$(`<img src="https://d.top4top.net/p_1214i8rn51.gif"class="fr"style="width:100%;margin-top: 1px;"> `).insertBefore('.nav-tabs');
+$(`<a href="rules.html"class="amirdonity"style="padding: 1px 5px 3px;margin-top: -160px;width: 35%;float: left;position: relative;margin-right: -20px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;">بنات اهلنا</a> <a href="rules.html"class="amirdonity"style="padding: 1px 5px 3px;margin-top: -132px;width: 35%;float: left;position: relative;margin-right: -20px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;">الـقـوانـيـن</a><a href="Shortcut.html"class="amirdonity"style="padding: 1px 5px 3px;width: 35%;margin-top: -107px;float: left;position: relative;margin-right: -20px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;">الاشـتـراكـات</a> <a href="Shortcut.html"class="amirdonity"style="padding: 1px 5px 3px;width: 35%;margin-top: -82px;float: left;position: relative;margin-right: -20px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;">أتـصـال بـنـا</a> <a href="Shortcut.html"class="amirdonity"style="padding: 1px 5px 3px;width: 35%;margin-top: -50px;float: left;margin-right: -20px;position: relative;margin-right: 1px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;">☪︎ ɑ̈ɹ̣ɹ̤බɺȊ     ☪︎</a>`).insertBefore('div#tlogins .lonline.light.break');
+
+
+$('.nav-tabs').addClass('fl').css('width', '100%');
+$(`
+<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=sky-bold" />
+<script type="text/javascript" src="soon" /> <link rel="stylesheet"type="text/css"href="//www.fontstatic.com/f=jazeera-light,jazeera"/> <script type="text/javascript"src="soon"/> <link rel="stylesheet"type="text/css"href="//www.fontstatic.com/f=sukar"/> <link rel="stylesheet"href="https://raw.githack.com/abbose/as.js/master/mnm.css"> `).insertBefore('body');
+
+$(` <center><div><marquee direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" > حياكم في حائط بنات اهلنا  الحائط للنشر فقط خلاف ذالك سيتم سحب لايكات شكرا لكم </marquee></font></div><center>`).insertBefore('div#d2bc'); 
+$(".u-pic" ).css( "border-radius", " 20px 20px 20px 20px" );
+$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '2px solid #6BD7E3',borderLeft: '2px solid #6BD7E3',borderTop: '1px solid #188C81',borderBottom: '1px solid #188C81',paddingTop: '2px',borderRadius: '10px'});
+
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '2px solid #4F565E',borderLeft: '2px solid #4F565E',borderTop: '.40px solid #54B4CC',borderBottom: '.50px solid #54B4CC',paddingTop: '2px',borderRadius: '10px'}); 
+$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '2px solid #08FFFF',borderLeft: '2px solid #188C81',borderTop: '1px solid #188C81',borderBottom: '1px solid #08FFFF',paddingTop: '2px',borderRadius: '10px'});$(".corner.fa.fa-user.label.label-primary.fr.uc").css("border-radius","15px")
+
+
+
+
+if(location.protocol!='https:'){location.protocol='https:';}
+
+$(` <center><div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-music btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp; 🔊 • ألـآغــاني  • 🔉 &nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>  🔝 𝖬𝖨𝖷 𝖡𝖮𝖷 🔝 </p><audio controls loop="" play=""><source src="https://c.top4top.net/m_11774j9z80.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
+
+$(` <center><div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-music btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp; 🔊 🎼 ⧼м⧽⧼υ⧽⧼s⧽⧼ɪ⧽⧼ĸ⧽ 🎼 🔊 &nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>  🎵 sɪsτyм sσиɢ 🎵 </p><audio controls loop="" play=""><source src="https://c.top4top.net/m_11774j9z80.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" );
+$(".border.corner").css("border-radius","24px 24px 24px 24px");
+
+$.getScript('https://loveriraq.000webhostapp.com/iraq3.js');
